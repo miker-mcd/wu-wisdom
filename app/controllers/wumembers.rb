93fd0cd435e48fb5d870ca797fec83d5b8f9a69b
Michael McDonald <michaelrmcd@gmail.com>
2017-09-18 10:29:00 -0700

    Update sleep statement for routes

--- app/controllers/wumembers.rb
+++ app/controllers/wumembers.rb
@@ -1,6 +1,8 @@
 require 'json'
 require 'rest-client'
 
+SLEEP_TIME = 0.5
+
 get '/wumembers' do
   @wumembers = Wumember.all
   erb :"/wumembers/index"
@@ -9,7 +11,7 @@ end
 get '/wumembers/:id' do
   @wumember = Wumember.find(params[:id])
   artist_response = RestClient.get("http://api.musixmatch.com/ws/1.1/artist.albums.get", params: {artist_id: "#{@wumember.artist_id}", apikey: "#{ENV["MUSIXMATCH_API_KEY"]}"})
-  sleep 1
+  sleep SLEEP_TIME
 
   parsed_artist_response = JSON.parse(artist_response.body)
   artist_album_ids = parsed_artist_response["message"]["body"]["album_list"].map {|entry| entry["album"]["album_id"]}
@@ -17,7 +19,7 @@ get '/wumembers/:id' do
   album_track_requests = []
   artist_album_ids.each do |album_id|
     album_track_requests << RestClient.get("http://api.musixmatch.com/ws/1.1/album.tracks.get", params: {album_id: "#{album_id}", apikey: "#{ENV["MUSIXMATCH_API_KEY"]}"})
-    sleep 1
+    sleep SLEEP_TIME
   end
 
   parsed_tracks_requests = []
@@ -33,7 +35,7 @@ get '/wumembers/:id' do
   lyrics_requests = []
   album_track_ids.flatten.each do |track_id|
     lyrics_requests << RestClient.get("http://api.musixmatch.com/ws/1.1/track.lyrics.get", params: {track_id: "#{track_id}", apikey: "#{ENV["MUSIXMATCH_API_KEY"]}"})
-    sleep 1
+    sleep SLEEP_TIME
   end
 
   parsed_lyrics_requests = []
